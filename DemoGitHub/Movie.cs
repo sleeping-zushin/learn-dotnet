0fa65b3b8eb34b40ffa99daa4b6cc76d2b1a61d5
Dat LT <datlt.mdc@gmail.com>
2025-09-05 14:44:37 +0700

    first commit

---
+++ DemoGitHub/Movie.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGitHub
+{
+    internal class Movie
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+        public double Rating { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} ({Year}) - {Genre} - Rating: {Rating}";
+        }
+
+    }
+}
---
+++ DemoGitHub/Program.cs
@@ -0,0 +1,17 @@
+﻿using DemoGitHub;
+
+List<Movie> movies = new List<Movie>
+        {
+            new Movie { Title = "Inception", Genre = "Sci-Fi", Year = 2010, Rating = 8.8 },
+            new Movie { Title = "The Dark Knight", Genre = "Action", Year = 2008, Rating = 9.0 },
+            new Movie { Title = "Interstellar", Genre = "Sci-Fi", Year = 2014, Rating = 8.6 },
+            new Movie { Title = "Parasite", Genre = "Thriller", Year = 2019, Rating = 8.6 },
+            new Movie { Title = "Avengers: Endgame", Genre = "Superhero", Year = 2019, Rating = 8.4 },
+            new Movie { Title = "The Godfather", Genre = "Crime", Year = 1972, Rating = 9.2 },
+            new Movie { Title = "Forrest Gump", Genre = "Drama", Year = 1994, Rating = 8.8 }
+        };
+
+foreach (var m in movies)
+{
+    Console.WriteLine(m);
+}
